58e5b4dc49fcdb7d70ed8d5035cad4f87b1ed397
PMHStudio <pmhstudio.pmh@gmail.com>
2019-07-10 21:11:40 +0900

    Viewer Upgraded

--- src/index.js
+++ src/index.js
@@ -1,29 +1,38 @@
-const electron = require('electron').remote
+const opn = require('opn')
 const superagent = require('superagent')
+const render = document.getElementById('render')
+
+document.addEventListener('keypress', (e) => {
+  if (e.code === 'Slash') window.location.replace('./nsfw.html')
+})
 
 function start () {
-  document.getElementById('go').disabled = true
+  render.innerHTML = ''
   let choice = document.getElementById('select').value
-  superagent.get('https://nekobot.xyz/api/image?type=' + choice, (err, res) => {
-    if (err) console.log(err)
-    let n = new electron.BrowserWindow({
-      frame: false,
+  for (let c = 0; c < 100; c++) {
+    superagent.get('https://nekobot.xyz/api/image?type=' + choice, (err, res) => {
+      if (err) console.error(err)
+  
+      render.innerHTML += renderer(res.body.message)
     })
-    n.setMenu(null)
-    n.loadURL(res.body.message)
-    copyTextToClipboard(res.body.message)
-    document.getElementById('go').disabled = false
-  })
+  }
 }
 
-function copyTextToClipboard (text) {
-  if (!navigator.clipboard) {
-    fallbackCopyTextToClipboard(text)
-    return
-  }
-  navigator.clipboard.writeText(text).then(() => {
-    console.log('Async: Copying to clipboard was successful!')
-  }, (err) => {
-    console.error('Async: Could not copy text: ', err)
-  });
+function renderer (url) {
+  return `<div class="card mb-3">
+<div class="row no-gutters">
+  <div class="col-md-4">
+    <img src="${url}" class="card-img" alt="${url}">
+  </div>
+  <div class="col-md-8">
+    <div class="card-body">
+      <h5 class="card-title">${url}</h5>
+      <button class="btn btn-primary" onclick="opn('${url}')">Open With Browser</button>
+      <p class="card-text"><small class="text-muted">Powered By NekoBot.xyz</small></p>
+    </div>
+  </div>
+</div>
+</div>`
 }
+
+
